d06a6858b6fb5510d089bdeb033a92923022e158
Alexandre Bellandi <bellandi@gmail.com>
2022-03-20 16:24:56 -0300

    update category

--- backend/src/main/java/com/ajblima/dscatalog/resource/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/ajblima/dscatalog/resource/exceptions/ResourceExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.ajblima.dscatalog.services.exceptions.EntityNotFoundException;
+import com.ajblima.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandartError> entityNotFound(EntityNotFoundException e, HttpServletRequest request)
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request)
 	{
 		StandartError err = new StandartError();
 		err.setTimestamp(Instant.now());
