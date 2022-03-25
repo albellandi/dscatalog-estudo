f6b7f0215eb122ed1ecd24dfa44b0dc8af752393
Alexandre Bellandi <bellandi@gmail.com>
2022-03-25 17:00:49 -0300

    Test Product

---
+++ backend/src/test/java/com/ajblima/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,39 @@
+package com.ajblima.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.ajblima.dscatalog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExist() {
+		Long existingId = 1L;
+		repository.deleteById(existingId);
+		
+		Optional<Product> result = repository.findById(existingId);
+		
+        Assertions.assertFalse(result.isPresent());
+		
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		
+		Long nonExistingId = 1000L;
+		
+        Assertions.assertThrows(EmptyResultDataAccessException.class, ()-> repository.deleteById(nonExistingId));
+		
+	}
+	
+}
