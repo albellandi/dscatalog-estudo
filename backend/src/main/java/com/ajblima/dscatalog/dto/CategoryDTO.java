3f73b43b96b22ffb6e976bb214d4894600ee2b8d
Alexandre Bellandi <bellandi@gmail.com>
2022-03-25 11:24:54 -0300

    validar categoryDTO

--- backend/src/main/java/com/ajblima/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/ajblima/dscatalog/dto/CategoryDTO.java
@@ -7,7 +7,7 @@ import com.ajblima.dscatalog.entities.Category;
 public class CategoryDTO  implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
-	
+	//teste
 	private Long id;
 	private String name;
 	
