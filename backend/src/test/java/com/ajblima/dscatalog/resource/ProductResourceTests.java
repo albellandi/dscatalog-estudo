4ac34cc4f729081d0ae0e9e2266f185150dfcccc
Alexandre Bellandi <bellandi@gmail.com>
2022-03-29 23:47:37 -0300

    Tests ProductResource

--- backend/src/test/java/com/ajblima/dscatalog/resource/ProductResourceTests.java
+++ backend/src/test/java/com/ajblima/dscatalog/resource/ProductResourceTests.java
@@ -2,8 +2,12 @@ package com.ajblima.dscatalog.resource;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +26,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.ajblima.dscatalog.dto.ProductDTO;
 import com.ajblima.dscatalog.services.ProductService;
+import com.ajblima.dscatalog.services.exceptions.DataBaseException;
 import com.ajblima.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.ajblima.dscatalog.tests.Factory;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -36,11 +41,12 @@ public class ProductResourceTests {
 	private ProductService service;
 	
 	private ProductDTO productDTO;
+	private ProductDTO productDTOInsert;
 	private PageImpl<ProductDTO> page;
 	
 	private Long existingId;
 	private Long nonExistingId;
-	
+	private Long dependentId;
 	@Autowired
 	private ObjectMapper objectMapper;
 	
@@ -49,8 +55,10 @@ public class ProductResourceTests {
 		
 		existingId = 1L;
 		nonExistingId = 2L;
+		dependentId = 3L;
 		
 		productDTO = Factory.CreateProductDTO();
+		productDTOInsert = Factory.CreateProductDTO();
 		page = new PageImpl<>(List.of(productDTO));
 		
 		when(service.findAllPaged(any())).thenReturn(page);
@@ -60,6 +68,46 @@ public class ProductResourceTests {
 		
 		when(service.update(eq(existingId), any())).thenReturn(productDTO);		
 		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+		
+		doNothing().when(service).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+		doThrow(DataBaseException.class).when(service).delete(dependentId);
+		
+		when(service.insert(any())).thenReturn(productDTO);
+	}
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExists() throws Exception{
+		ResultActions result = mockMvc.perform(delete("/products/{id}",existingId).
+				 accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deleteShouldReturnNotFoundtWhenIdDoesNotExist() throws Exception{
+		ResultActions result = mockMvc.perform(delete("/products/{id}",nonExistingId).
+				 accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void insertShouldReturnProductDTOCreated() throws Exception{
+		
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTOInsert);
+				
+		ResultActions result = 
+				 mockMvc.perform(post("/products/")
+				 .content(jsonBody)	
+				 .contentType(MediaType.APPLICATION_JSON)
+				 .accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
 	}
 	
 	@Test
