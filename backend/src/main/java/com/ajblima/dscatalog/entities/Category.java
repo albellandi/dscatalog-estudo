b35bfd836227f5104bda7b6aa72a96d647a0c223
Alexandre Bellandi <bellandi@gmail.com>
2022-04-03 10:49:43 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/ajblima/dscatalog/entities/Category.java
+++ backend/src/main/java/com/ajblima/dscatalog/entities/Category.java
@@ -2,16 +2,21 @@ package com.ajblima.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 @Table(name = "tb_category")
@@ -30,6 +35,8 @@ public class Category implements Serializable  {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {		
 	}
@@ -71,6 +78,10 @@ public class Category implements Serializable  {
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
